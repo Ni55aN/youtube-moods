2bc16f5dbfe24d2dde1a6149a93e47f953ea380f
Ni55aN <huccah@gmail.com>
2020-12-23 18:40:23 +0200

    detect languages

--- src/analyze.ts
+++ src/analyze.ts
@@ -1,5 +1,6 @@
-import { SentimentAnalyzer, SentimentRecord } from './node-nlp'
+import franc from 'franc'
 import { mean, median } from 'stats-lite'
+import { SentimentAnalyzer, SentimentRecord } from './node-nlp'
 import { readData } from './csv'
 
 export  async function analyze(input: string, language: string) {
@@ -19,5 +20,15 @@ export  async function analyze(input: string, language: string) {
     console.log('median: ', median(sentimentsScores))
     console.log('number of positive: ', sentimentsScores.filter(s => s > 0).length)
     console.log('number of negative: ', sentimentsScores.filter(s => s < 0).length)
-    
+
+    const languages = comments.map(comment => ({
+        comment,
+        lang: comment.textOriginal ? franc(comment.textOriginal) : null
+    }))
+    const uniqueLangs = Array.from(new Set(languages.map(l => l.lang)))
+    const langsUsage = uniqueLangs.map(lang => ({ lang, count: languages.filter(l => l.lang === lang).length }))
+    langsUsage.sort((a,b) => b.count - a.count)
+
+    console.log('===\nLanguages usage:')
+    console.log(langsUsage.map(item => `${item.lang} -> ${item.count}`).join('\n'))
 }
